13e3813098fb16ef22b5ad36076db066d01122d7
ProdtoPodcast <kharl.orlandini@urbixhub.com>
2026-01-21 13:30:03 -0500

    SCRUM-349 Kharl DevOPS Ramp up

---
+++ kharl-airbnb/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { Nunito } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kharl Airbnb Clone",
+  description: "Airbnb Clone",
+};
+
+const nunito = Nunito({
+subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>{children}</body>
+    </html>
+  );
+}
